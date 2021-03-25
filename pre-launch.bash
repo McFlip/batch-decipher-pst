bbdf2323b970508e56e60b61eb139b486d4d434e
McFlip <gradydenton@yahoo.com>
2021-03-25 14:46:22 -0400

    start docker in pre-launch script

--- pre-launch.bash
+++ pre-launch.bash
@@ -4,5 +4,8 @@
 # mount public share - workaround for node user accessing root owned volume
 sudo mount -t cifs -o username=samba_user,uid=$(id -u),gid=$(id -g) //localhost/public $(pwd)/public
 
+# start the docker service
+sudo systemctl start docker
+
 # set permission on docker REST API Unix socket
 sudo chmod 666 /var/run/docker.sock 
