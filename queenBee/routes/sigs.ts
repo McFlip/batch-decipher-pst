59ed635b6e5a97a9713ca08b313e8773f295d43a
McFlip <gradydenton@yahoo.com>
2021-10-06 18:51:58 -0400

    nuke midware to clean out previous uploads

--- queenBee/routes/sigs.ts
+++ queenBee/routes/sigs.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express'
+import { NextFunction, Request, Response } from 'express'
 import * as sigsController from '../controllers/sigs'
 import multer from 'multer'
 import debug from 'debug'
 import { pathValidator } from '../util/pathvalidator'
+import fs from 'fs'
+import path from 'path'
 
 const debugSig = debug('sig')
 const router = Router()
+// file uploads
 const storage = multer.diskStorage({
 	destination: (req, file, cb) => {
 		const { caseId } = req.params
@@ -15,9 +19,16 @@ const storage = multer.diskStorage({
 	}
 })
 const upload = multer({ storage })
+// clean out the upload dir
+export const nuke = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+	const { caseId } = req.params
+	const pstPath = `/app/workspace/${caseId}/sigsPSTs`
+	fs.readdirSync(pstPath).forEach(f => fs.rmSync(path.join(pstPath, f)))
+	next()
+}
 
 // Upload PST files
-router.post('/upload/:caseId', upload.array('pst'), sigsController.uploadSigsPst)
+router.post('/upload/:caseId', nuke, upload.array('pst'), sigsController.uploadSigsPst)
 // Process Signed Email
 router.post('/', sigsController.processSigs)
 // Get certs from allCerts.txt
