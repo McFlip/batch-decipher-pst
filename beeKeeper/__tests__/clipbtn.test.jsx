33180032386b127cac23ccb7692818e4fdab2025
McFlip <gradydenton@yahoo.com>
2022-06-10 17:19:51 -0400

    clipboard btn test

---
+++ beeKeeper/__tests__/clipbtn.test.jsx
@@ -0,0 +1,21 @@
+import ClipBtn from '../components/clipbtn'
+import '@testing-library/jest-dom'
+import { fireEvent, render } from '@testing-library/react'
+import { act } from 'react-dom/test-utils'
+
+// store clipboard contents
+let clipboard = ''
+// mock navigator.clipboard.writeText
+Object.defineProperty(navigator, "clipboard", {
+  value: {
+    writeText: (txt) => clipboard = txt,
+  },
+})
+
+describe('Copy to Clipboard', () => {
+	it('copies a str from the prop txtToCopy to the clipboard', () => {
+		const { getByRole } = render(<ClipBtn txtToCopy="Swear to me!" />)
+		act(() => fireEvent.click(getByRole('button')))
+		expect(clipboard).toBe('Swear to me!')
+	})
+})
