57641c8e6ebf2026c2b9b68c598649ec3fcd81bd
McFlip <gradydenton@yahoo.com>
2023-05-22 17:24:57 -0400

    rm nextauth jest fix test

--- beeKeeper/__tests__/pages/editcase.jsx
+++ beeKeeper/__tests__/pages/editcase.jsx
@@ -5,44 +5,6 @@ import userEvent from "@testing-library/user-event"
 import { useRouter } from "next/router"
 import testCases from "fixtures/cases"
 
-/**
- * @todo Jest is broken due to jose dep in next-auth in unstable_getServerSession
- */
-// Due to Jest transformer issues, we mock next-auth's useSession hook directly:
-const mockSession = {
-  expires: new Date(Date.now() + 2 * 86400).toISOString(),
-  user: { name: "admin" },
-}
-jest.mock("next-auth/react", () => {
-  const originalModule = jest.requireActual("next-auth/react")
-  return {
-    __esModule: true,
-    ...originalModule,
-    useSession: jest.fn(() => ({
-      data: mockSession,
-      status: "authenticated",
-    })),
-  }
-})
-// Reference: https://github.com/nextauthjs/next-auth/discussions/4185#discussioncomment-2397318
-// We also need to mock the whole next-auth package, since it's used in
-// our various pages via the `export { getServerSideProps }` function.
-jest.mock("next-auth", () => ({
-  __esModule: true,
-  default: jest.fn(),
-  unstable_getServerSession: jest.fn(
-    () =>
-      new Promise((resolve) => {
-        resolve({
-          expiresIn: undefined,
-          loggedInAt: undefined,
-          someProp: "someString",
-        })
-      })
-  ),
-}))
-// Reference: https://github.com/nextauthjs/next-auth/issues/4866
-
 // mocking alert messages
 let alertMsg = []
 window.alert = jest.fn((msg) => alertMsg.push(msg))
